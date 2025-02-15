9d163745b109fcc3338c1aee5a7799317bfda98d
amir0hd4@gmail.com <amir0hd4@gmail.com>
2025-02-15 17:13:51 +0330

    edge detection

--- notebook/edge_detection.py
+++ notebook/edge_detection.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-image = cv.imread("./notebook/images/road_in_norway.jpg", 0)
+image = cv.imread("images/Road_in_Norway.jpg", 0)
 image_noise_removed = cv.GaussianBlur(image, (3, 3), 0)
 
 # Laplacian
@@ -17,8 +17,8 @@ sobely = cv.Sobel(image_noise_removed, cv.CV_64F, 0, 1, ksize=5)
 # Canny Edge Detection
 canny = cv.Canny(image_noise_removed,  100, 200)
 
-plt.subplot(2, 2, 1), plt.imshow(image, cmap='gray')
-plt.title("Original")
+plt.subplot(2, 2, 1), plt.imshow(laplacian, cmap='gray')
+plt.title("Laplacian")
 plt.subplot(2, 2, 2), plt.imshow(canny, cmap='gray')
 plt.title("Canny Edge Detection")
 plt.subplot(2, 2, 3), plt.imshow(sobelx, cmap='gray')
